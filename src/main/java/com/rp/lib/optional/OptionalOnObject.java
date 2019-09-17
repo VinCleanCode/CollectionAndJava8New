9b18bf09aee5b526463379cf05dcf3f43f61a961
vincentmylee <vincentmylee@gmail.com>
2019-09-18 01:04:09 +0800

    Optional parameter, create item object form JSON

--- src/main/java/com/rp/lib/optional/OptionalOnObject.java
+++ src/main/java/com/rp/lib/optional/OptionalOnObject.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.*;
+import java.util.Optional;
 
 import static org.apache.logging.log4j.core.util.Loader.getClassLoader;
 
@@ -16,5 +17,13 @@ public class OptionalOnObject {
         File JSONFile = new File(getClassLoader().getResource("item.json").getFile());
         Item item = (Item) JSONtoObj.parse(JSONFile, Item.class.getName());
         log.info("item Action: {}",item.get("Action"));
+        Optional<Integer> tID = item.getInt("TransactionNumber");
+        log.info(tID);
+
+        Optional<Double> price = item.getDouble("UnitPrice");
+        log.info(price);
+
+        Optional<Double> discount = item.getDouble("Discount");
+        log.info(discount);
     }
 }
--- src/main/java/com/rp/lib/tuple/Item.java
+++ src/main/java/com/rp/lib/tuple/Item.java
@@ -1,5 +1,24 @@
 package com.rp.lib.tuple;
 
+import java.util.Optional;
+
 public class Item extends JSONtoObj {
 
+    public Optional<Integer> getInt(String k){
+        Optional<Integer> optInt = Optional.empty();
+        Object obj = this.get(k);
+        if(obj instanceof Integer) {
+            optInt = Optional.ofNullable((Integer) obj);
+        }
+        return optInt;
+    }
+
+    public Optional<Double> getDouble(String k){
+        Optional<Double> optDouble = Optional.empty();
+        Object obj = this.get(k);
+        if(obj instanceof Double) {
+            optDouble = Optional.ofNullable((Double) obj);
+        }
+        return optDouble;
+    }
 }
