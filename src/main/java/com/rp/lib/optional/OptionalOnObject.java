dbf51e30f691add56eb174042505835af18958e3
vincentmylee <vincentmylee@gmail.com>
2019-09-20 21:57:49 +0800

    add handle customer Object

--- src/main/java/com/rp/lib/optional/OptionalOnObject.java
+++ src/main/java/com/rp/lib/optional/OptionalOnObject.java
@@ -1,29 +1,44 @@
 package com.rp.lib.optional;
 
-import com.rp.lib.tuple.JSONtoObj;
+import com.rp.lib.tuple.Customer;
 import com.rp.lib.tuple.Item;
+import com.rp.lib.tuple.JSONtoObj;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.*;
 import java.util.Optional;
+import org.apache.logging.log4j.core.util.Loader;
 
-import static org.apache.logging.log4j.core.util.Loader.getClassLoader;
+import java.io.File;
+import java.io.IOException;
 
 public class OptionalOnObject {
     private static Logger log = LogManager.getLogger();
+    private static Loader loader;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        File JSONFile = new File(getClassLoader().getResource("item.json").getFile());
+        processItem("item.json");
+        processCustomer("customer.json");
+    }
+
+    private static void processItem(String JSONFileName) throws ClassNotFoundException, IOException {
+        File JSONFile = new File(loader.getClassLoader().getResource(JSONFileName).getFile());
         Item item = (Item) JSONtoObj.parse(JSONFile, Item.class.getName());
-        log.info("item Action: {}",item.get("Action"));
+
+        log.info("item: {}",item.toString());
+
         Optional<Integer> tID = item.getInt("TransactionNumber");
         log.info(tID);
-
         Optional<Double> price = item.getDouble("UnitPrice");
         log.info(price);
-
         Optional<Double> discount = item.getDouble("Discount");
         log.info(discount);
+    }
+
+    private static void processCustomer(String JSONFileName) throws ClassNotFoundException, IOException {
+        File JSONFile = new File(loader.getClassLoader().getResource(JSONFileName).getFile());
+        Customer customer = (Customer) JSONtoObj.parse(JSONFile, Customer.class.getName());
+
+        log.info("customer: {}",customer.toString());
+        log.info(customer.get("phoneNumber"));
     }
 }
---
+++ src/main/java/com/rp/lib/tuple/Customer.java
@@ -0,0 +1,7 @@
+package com.rp.lib.tuple;
+
+import java.util.Optional;
+
+public class Customer extends JSONtoObj {
+  Optional<String> address = Optional.empty();
+}
